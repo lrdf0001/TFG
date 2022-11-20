bdb106952455d93cd46c6c7b665ab3e51de0f444
lrdf0001 <lrdf0001@red.ujaen.es>
2022-11-20 22:18:19 +0100

    Caja Zapatos

--- ThreeJS/cubito.js
+++ ThreeJS/cubito.js
@@ -1,32 +1,85 @@
-//Escena
+//========================= Escena ============================
 const scene = new THREE.Scene()
 
-//Cubo
-const geometry = new THREE.BoxGeometry( 1, 1, 1 )
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
-const cube = new THREE.Mesh( geometry, material )
-scene.add( cube )
 
-//Camara
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+//========================== Ejes =============================
+
+//-------------------------- Eje X ----------------------------
+	const ejeXvertices = []
+	ejeXvertices.push( new THREE.Vector3( - 100, 0, 0 ) )
+	ejeXvertices.push( new THREE.Vector3(  100, 0, 0 ) )
+	const colorEjeX = new THREE.LineBasicMaterial( { color:  0x943126 } )
+
+	const geometriaX = new THREE.BufferGeometry().setFromPoints( ejeXvertices )
+	const ejeX = new THREE.Line( geometriaX, colorEjeX )
+
+	scene.add(ejeX)
+
+//-------------------------- Eje Y ----------------------------
+	const ejeYvertices = []
+	ejeYvertices.push( new THREE.Vector3( 0, -100, 0 ) )
+	ejeYvertices.push( new THREE.Vector3(  0, 100, 0 ) )
+	const colorEjeY = new THREE.LineBasicMaterial( { color:   0x2ecc71  } )
+
+	const geometriaY = new THREE.BufferGeometry().setFromPoints( ejeYvertices )
+	const ejeY = new THREE.Line( geometriaY, colorEjeY )
+
+	scene.add(ejeY)
+
+//-------------------------- Eje Z ----------------------------
+	const ejeZvertices = []
+	ejeZvertices.push( new THREE.Vector3( 0, 0, -100 ) )
+	ejeZvertices.push( new THREE.Vector3(  0, 0, 100 ) )
+	const colorEjeZ = new THREE.LineBasicMaterial( { color:   0x3498db  } )
+	
+	const geometriaZ = new THREE.BufferGeometry().setFromPoints( ejeZvertices )
+	const ejeZ = new THREE.Line( geometriaZ, colorEjeZ )
+
+	scene.add(ejeZ)
+
+
+//========================= Camara =============================
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 15 )
+camera.position.x = 2
+camera.position.y = 2
 camera.position.z = 5
+camera.lookAt(0,0,0)
 scene.add(camera)
 
-//Renderer
+
+//========================== Caja =============================
+const geometriaCaja = new THREE.BoxGeometry( 1, 1, 2 )
+const materialCaja = new THREE.MeshBasicMaterial( { color:    0x239b56   } )
+const caja = new THREE.Mesh( geometriaCaja, materialCaja )
+scene.add( caja )
+
+const geometriaTapa = new THREE.BoxGeometry( 1.15, 0.25, 2.15 )
+const materialTapa = new THREE.MeshBasicMaterial( { color:    0x239b56   } )
+const tapa = new THREE.Mesh( geometriaTapa, materialTapa )
+tapa.translateY(0.5)
+scene.add( tapa )
+
+
+//========================== Render =============================
 const canvas = document.querySelector('#mi_canvas')
 const renderer = new THREE.WebGLRenderer({canvas: canvas})
 renderer.setSize( window.innerWidth, window.innerHeight )
 
 renderer.render(scene, camera)
 
+//------------------------- Visualiza ----------------------------
 
-//Refrescar ventana
 	function animate() {
 		requestAnimationFrame( animate )
 
-		cube.rotation.x += 0.01
-		cube.rotation.y += 0.01
-
+		caja.rotation.x += 0.01
+		caja.rotation.y += 0.01
+		
+		tapa.translateY(-0.5)
+		tapa.rotation.x += 0.01
+		tapa.rotation.y += 0.01
+		tapa.translateY(0.5)
+		
 		renderer.render( scene, camera )
 		renderer.setSize( window.innerWidth, window.innerHeight )
 	};
