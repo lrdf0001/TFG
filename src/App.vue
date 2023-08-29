e1c6a02c850da250c213ea8766fe63c035152574
lrdf0001 <lrdf0001@red.ujaen.es>
2023-08-29 17:15:15 +0200

    Arreglo NavBar

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import Footer from './components/Footer.vue'
   <header>
       <div>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <!--<div class="container-fluid">-->
+        <div class="container-fluid">
           <a class="navbar-brand" href="#">TFG</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -50,7 +50,7 @@ import Footer from './components/Footer.vue'
               
             </ul>
           </div>
-        <!--</div>-->
+        </div>
       </nav>
       </div>
   </header>
