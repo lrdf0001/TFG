5a4c287af9373f2735fd5f11b090c52791cc8211
lrdf0001 <lrdf0001@red.ujaen.es>
2023-08-29 16:45:52 +0200

    Prueba Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,12 @@
 <template>
   
 <br/>
+<!---
 <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
     <div class="container text-center">
       <small>Herramienta para la docencia de Informática Gráfica utilizando WebGL - 2023 &copy; Luis René Domínguez Fuentes</small>
     </div>
 </footer>
+-->
 
 </template>
